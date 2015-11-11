ed05883f05255a69264a67dcf4dc0e71a995879a
Arunram A <arunram.atma@gmail.com>
2015-11-11 23:40:54 +0530

    Zipper - Progress
    Zipper for Tree data structure done.

--- 14_zippers.hs
+++ 14_zippers.hs
@@ -102,6 +102,11 @@ type BrdCrmbs a = [Crumb a]
 
 goLeft' :: (Tree a, BrdCrmbs a) -> (Tree a, BrdCrmbs a)
 goLeft' (Node x l r, bs) = (l, (LeftCrumb x r) : bs)
+-- goLeft' gives a Tree and BrdCrmbs as a tuple
+-- If you start at the root, the first element of goLeft' is the tree which 
+-- is arrived taking a left from the root and the BrdCrmbs list will have a 
+-- single element of data type "Crumb a", which is LeftCrumb (rootNode) (tree
+-- formed if taken not taken left)
 
 goRight' :: (Tree a, BrdCrmbs a) -> (Tree a, BrdCrmbs a)
 goRight' (Node x l r, bs) = (r, (RightCrumb x l) : bs)
@@ -110,4 +115,74 @@ goRight' (Node x l r, bs) = (r, (RightCrumb x l) : bs)
 goUp' :: (Tree a, BrdCrmbs a) -> (Tree a, BrdCrmbs a)
 goUp' (t, (LeftCrumb x r) : bs) = (Node x t r, bs)
 goUp' (t, (RightCrumb x l) : bs) = (Node x l t, bs)
+
+{-------------------------------------------------------------------------------
+  goUp' takes a tuple containing two elements: the current tree and the 
+  list of Crumbs i.e BrdCrmbs. Each Crumb contain information about the element
+  which is just the parent of the current tree and the tree which is not taken.
+  So, to get a tree at one step up, form a tree using the first element from 
+  BrdCrmbs representing the root node of the current tree and the LeftCrumb
+  or RightCrumb (the tree that is not traversed with the given root node)
+-------------------------------------------------------------------------------}
+
+-- Todo:
+-- Failure case not handled for goUp' if already at the top most node
+-- Need to implement using 'Maybe'
+
+{-------------------------------------------------------------------------------
+
+    So, (Tree a, BrdCrmbs a) contain information about a particular node and
+    its parent, along with its other child.  So, the focus is exactly on the 
+    node being examined, with complete information about its surroundings
+    
+    Such, a focussed sub part of a complex data structures is Zipper
+-------------------------------------------------------------------------------}
+
+type Zipper a = (Tree a, BrdCrmbs a)
+
+-- Manipulating the trees under focus
+-- Apply a function on a given node.
+modify :: (a -> a) -> Zipper a -> Zipper a
+modify f (Node x l r, bs) = (Node (f x) l r, bs)
+modify f (Empty, bs) = (Empty, bs)
+
+-- From top, go left, go right replace the node to 'M'
+newFocus = modify (\_ -> 'M') (goRight' (goLeft' (charTree, [])))
+-- Here the focus will be on the node obtained by turning a left and right
+-- But, the breadcrumbs has information about the entire tree.
+
+-- the same, using (-:)
+sameNewFocus = (charTree, []) -: goLeft' -: goRight' -: modify (\_ -> 'M')
+
+-- Let us get on to another focus, by going one up
+newFocus2 = goUp' newFocus
+
+-- Change the element at newFocus2 to something else (say 'X')
+changedNewFocus2 = modify (\_ -> 'X') newFocus2
+
+-- same using (-:), directly from newFocus, instead of getting from newFocus2
+sameChangedNewFocus2 = newFocus -: goUp' -: modify (\_ -> 'X') 
+
+{-------------------------------------------------------------------------------
+    Attaching a tree, at a given node
+-------------------------------------------------------------------------------}
+
+attach :: Tree a -> Zipper a -> Zipper a
+attach t (_, bs) = (t, bs)
+-- Effectively means:
+-- Focus on a given node (getting a Zipper); Do not care about what is there in 
+-- the node; get a tree and attach there, and present the entire tree as Zipper
+
+farLeft = (charTree, []) -: goLeft' -: goLeft' -: goLeft' -: goLeft'
+zipperExtTree = farLeft -: attach (Node 'X' Empty Empty)
+-- zipperExtTree contains entire tree information with the new small tree
+-- attached at the far left of the charTree
+
+-- Use recursion to get the top most node
+topMost :: Zipper a -> Zipper a
+topMost (t, []) = (t, [])               -- If bs empty, we are already at top!
+topMost z       = topMost (goUp' z)     -- Else, go up once!
+
+
+
 
